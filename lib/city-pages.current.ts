8a94532b2ab0f601dd78a6fb5f899a3519937f28
Jatin-ge <Jatin@growthescalators.com>
2026-08-29 23:12:16 +0530

    SEO: apply conservative city-claim normalization

--- lib/city-pages.current.ts
+++ lib/city-pages.current.ts
@@ -1,6 +1,7 @@
 import { CITY_PAGES as RAW_CITY_PAGES } from "./city-pages";
 import { normalizePracticeFacts } from "./practice-fact-normalizer";
+import { normalizeCityClaims } from "./city-claim-normalizer";
 
-export const CITY_PAGES: typeof RAW_CITY_PAGES = normalizePracticeFacts(
-  RAW_CITY_PAGES,
+export const CITY_PAGES: typeof RAW_CITY_PAGES = normalizeCityClaims(
+  normalizePracticeFacts(RAW_CITY_PAGES),
 );
